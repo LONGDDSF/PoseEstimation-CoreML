050f9c776ab867ff8b6c01b46fc97a79ca920974
chendongdong <chendongdong@dongyin.net.cn>
2020-03-17 23:54:23 +0800

    碰撞音效

--- PoseEstimation-CoreML/SpriteHero/HeroScene.swift
+++ PoseEstimation-CoreML/SpriteHero/HeroScene.swift
@@ -8,32 +8,51 @@
 
 import Foundation
 import SpriteKit
+import AVFoundation
+
+let enemyCategory: UInt32 = 0x1 << 0
+let fistCategory: UInt32 = 0x1 << 1
 
 class HeroScene: SKScene {
     
     var fistNode: SKSpriteNode!
-
+    var floorNode:SKSpriteNode!
+    var score: NSInteger = 0
+    
     override func didMove(to view: SKView) {
         
-        setupUI()
+        setupScene()
         
-        addScore()
-        
-        repeatEnemy()
+        addScoreLable()
         
         addfistNode()
+        
+        repeatEnemy()
+    
     }
     
-    func setupUI() {
+    func setupScene() {
         self.backgroundColor = SKColor(red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
         self.backgroundColor = SKColor.clear
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -1)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -1.3)
         self.physicsWorld.contactDelegate = self
     }
     
-    /// 分数
-    var score: NSInteger = 0
-    ///分数Label
+    // 声音
+    var audioPlayer : AVAudioPlayer = {
+         var player : AVAudioPlayer?
+         let mp3Path = Bundle.main.path(forResource: "11970", ofType: "mp3")   //bgm自己放一首进去就好
+         let pathURL = NSURL.fileURL(withPath: mp3Path!)
+         try? player = AVAudioPlayer(contentsOf: pathURL)
+         return player!
+     }()
+    
+    func playMusic() {
+        audioPlayer.numberOfLoops = 1
+        audioPlayer.play()
+    }
+    
+    // 分数
     lazy var scoreLabelNode:SKLabelNode = {
         let label = SKLabelNode(fontNamed: "MarkerFelt-Wide")
         label.zPosition = 100
@@ -41,11 +60,16 @@ class HeroScene: SKScene {
         return label
     }()
     
-    func addScore() {
+    func addScoreLable() {
         score = 0
         scoreLabelNode.text = String(score)
         self.addChild(scoreLabelNode)
         scoreLabelNode.position = CGPoint(x: self.frame.size.width - 30, y: self.frame.size.height - 50)
+    }
+    
+    func scorePlus() {
+        score += 1
+        scoreLabelNode.text = "\(score)" + "分"
     }
     
     // 添加一个敌人
@@ -63,7 +87,9 @@ class HeroScene: SKScene {
         // 初始位置
         enemy.position = CGPoint(x:CGFloat(randomX),y:winSize.height + enemy.size.height/2);
         enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
-
+        enemy.physicsBody?.categoryBitMask = enemyCategory
+        enemy.physicsBody?.contactTestBitMask = fistCategory
+        enemy.physicsBody?.collisionBitMask = fistCategory
         // 进场
         addChild(enemy)
     }
@@ -86,6 +112,10 @@ class HeroScene: SKScene {
         fistNode.physicsBody?.allowsRotation = false
         fistNode.physicsBody?.isDynamic = false
         
+        fistNode.physicsBody?.categoryBitMask = fistCategory
+        fistNode.physicsBody?.contactTestBitMask = enemyCategory
+        fistNode.physicsBody?.collisionBitMask = enemyCategory
+        
         addChild(fistNode)
     }
 }
@@ -105,12 +135,24 @@ extension HeroScene{
 extension HeroScene: SKPhysicsContactDelegate{
     
     func didBegin(_ contact: SKPhysicsContact) {
+        playMusic()
         
+        if contact.bodyA.node == fistNode ||
+            contact.bodyB.node == fistNode{
+            scorePlus()
+        }
     }
     
     func didEnd(_ contact: SKPhysicsContact) {
         
+        if contact.bodyA.node != fistNode{
+            contact.bodyA.node?.removeFromParent()
+        }
+        if contact.bodyB.node != fistNode{
+            contact.bodyB.node?.removeFromParent()
+        }
     }
 }
+
 
 
